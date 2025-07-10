261f30a6408373d6a41c76934eaa4c543dd6db55
Wagner de Souza França <wagnerdesouzafranca@gmail.com>
2025-07-10 11:19:41 -0300

    Exercicio01.java atualizado o nome e Exercicio02.java feito

--- estruturasRepetitivas/Exercicio01.java
+++ estruturasRepetitivas/Exercicio01.java
@@ -11,7 +11,7 @@ package estruturasRepetitivas;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Exercicio_01 {
+public class Exercicio01 {
 
 	public static void main(String[] args) {
 
