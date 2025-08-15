6ae5c9c1cdcca379d0ceeb84abbdf3a72d0098e5
Wagner de Souza França <wagnerdesouzafranca@gmail.com>
2025-08-15 19:19:08 -0300

    Exercicio feito

---
+++ topicosBasicos/Exemplo05.java
@@ -0,0 +1,15 @@
+package topicosBasicos;
+
+public class Exemplo05 {
+
+	public static void main(String[] args) {
+		/*
+		 * Comentários de blocos
+		 */
+
+		// Comentário de linha
+		
+		
+	}
+
+}
